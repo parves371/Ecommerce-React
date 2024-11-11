fa516b5c15209e150ae767cc4d8fee40a18fa9cd
md-parves <parves.dev@gmail.com>
2024-11-11 11:45:55 +0600

    add shope layout

---
+++ src/app/shop/layout.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/component/layout/Footer";
+import Header from "@/component/layout/Header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
